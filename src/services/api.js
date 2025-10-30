68be16f1c4f9ac23f4b362112e04c56152ab53b3
aguscastaneda <castaneda.agustin.et21.21@gmail.com>
2025-10-30 19:53:57 -0300

    Update API base URL to use environment variable

--- src/services/api.js
+++ src/services/api.js
@@ -2,12 +2,13 @@ import axios from 'axios';
 
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: import.meta.env.VITE_API_URL + '/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
   },
 });
+
 
 
 let navigateFunction = null;
